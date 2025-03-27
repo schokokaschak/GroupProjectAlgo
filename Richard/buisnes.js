62187821b2eaf198e3a89e312c09868582580f32
sovenok47 <sova28.2888@gmail.com>
2025-03-27 18:43:29 +0100

    lololo2

--- Richard/buisnes.js
+++ Richard/buisnes.js
@@ -1,19 +1,20 @@
 function calculate() {
     let income =
     Number(document.getElementById("income").value);
-    let income =
+    let expenses =
     Number(document.getElementById("totalExpenses").value);
 }
 
-let balence = income - totalExpenses;
+let balance = income - totalExpenses;
 
-function getAdvice(balance:)
-if(balance >= 500) {
-    return "Your finances are in order cousider investment opportunities"
-} else if (balance >= 0) {
-    return "You are in the black but you schould think about getting a part-time job"
-} else if (balance < 0 && balance > -500) {
-    return "You spend more than you earn, you should cut expenses"
-} else {
-    return "Your budget is in critical condition, if you don´t do something, you will soon not be adle, you will soon not be able to support yourself"
+function getAdvice(balance) {
+    if(balance >= 500) {
+        return "Your finances are in order cousider investment opportunities"
+    } else if (balance >= 0) {
+        return "You are in the black but you schould think about getting a part-time job"
+    } else if (balance < 0 && balance > -500) {
+        return "You spend more than you earn, you should cut expenses"
+    } else {
+        return "Your budget is in critical condition, if you don´t do something, you will soon not be adle, you will soon not be able to support yourself"
+    }
 }
