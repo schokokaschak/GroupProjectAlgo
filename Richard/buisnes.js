0e11ff57888c685d3208f30a043f9bd8e9f63c2b
sovenok47 <sova28.2888@gmail.com>
2025-04-03 19:24:17 +0200

    lololo4

--- Richard/buisnes.js
+++ Richard/buisnes.js
@@ -4,21 +4,20 @@ let RaschotButton = document.querySelector(".raschot")
 let SovetButton = document.querySelector(".sovet")
 let h2 = document.querySelector("h2")
 let income = document.querySelector(".income")
-let totalExpenses = document.querySelector(".totalExpenses")
+let totalExpenses = document.querySelector(".expensess")
 
-RaschotButton.addEventListener('click', function calculate() {
+
+
+SovetButton.addEventListener('click', function() {
     let income =
     Number(document.getElementById("income").value);
     let expenses =
-    Number(document.getElementById("totalExpenses").value);
-    let balance = income - totalExpenses;
-})
-
-SovetButton.addEventListener('click', function getAdvice(balance) {
+    Number(document.getElementById("expenses").value);
+    let balance = income - expenses;
     if(balance >= 500) {
         h2.innerHTML = `<h2>Your finances are in order cousider investment opportunities</h2>`
     } else if (balance >= 0) {
-        h2.innerHTML = `<h2>You are in the black but you schould think about getting a part-time job</h2>`
+        h2.innerHTML = `<h2>You are in the black but you schoguld think about getting a part-time job</h2>`
     } else if (balance < 0 && balance > -500) {
         h2.innerHTML = `<h2>You spend more than you earn, you should cut expenses</h2>`
     } else {
